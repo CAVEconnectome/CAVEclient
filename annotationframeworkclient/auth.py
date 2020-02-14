00c11213e550aaf0d547ec4b95906bd22ba018a8
Casey Schneider-Mizell <caseysm@gmail.com>
2020-02-13 18:30:45 -0800

    Small refactoring and bug fix

--- annotationframeworkclient/auth.py
+++ annotationframeworkclient/auth.py
@@ -15,19 +15,31 @@ class AuthClient(object):
         token=None,
         server_address=default_server_address,
     ):
-        if token is None:
-            if os.path.exists(token_file):
-                with open(token_file, "r") as f:
-                    token = json.load(f).get(token_key, None)
-        self._token = token
         self._token_file = token_file
         self._token_key = token_key
+
+        if token is None:
+            token = self._load_token(self._token_file, self._token_key)
+        self._token = token
+
         self._server_address = server_address
         self._default_endpoint_mapping = {"auth_server_address": self._server_address}
 
     @property
     def token(self):
         return self._token
+
+    def get(self, token_key):
+        self._token_key = token_key
+        self._token = self._load_token(self._token_file, self._token_key)
+
+    def _load_token(self, token_file, token_key):
+        if os.path.exists(token_file):
+            with open(token_file, "r") as f:
+                token = json.load(f).get(token_key, None)
+        else:
+            token = None
+        return token
 
     def get_new_token(self):
         auth_url = auth_endpoints["refresh_token"].format_map(
@@ -37,7 +49,7 @@ class AuthClient(object):
         print(txt)
         return None
 
-    def save_new_token(self, new_token=None, new_token_key="token", overwrite=False):
+    def save_new_token(self, new_token=None, new_token_key="token", overwrite=False, switch_token=True):
         if new_token is None:
             new_token = self.token
 
@@ -57,6 +69,10 @@ class AuthClient(object):
         secrets[new_token_key] = new_token
         with open(self._token_file, "w") as f:
             json.dump(secrets, f)
+
+        if switch_token:
+            self._token = new_token
+            self._token_key = new_token_key
 
     @property
     def request_header(self):
