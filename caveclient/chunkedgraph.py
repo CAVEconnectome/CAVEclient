371781584e688696e90dc4a46ee4678c5441268f
sdorkenw <sven.dorkenwald@googlemail.com>
2021-11-18 13:31:21 -0800

    minor changes to root_id conversion

--- caveclient/chunkedgraph.py
+++ caveclient/chunkedgraph.py
@@ -505,10 +505,12 @@ class ChunkedGraphClientV1(ClientBase):
         dict
             Dictionary describing the lineage graph and operations for the root id.
         """
-        try:
-            root_id = [int(r) for r in np.unique(root_id)]
-        except:
+        if isinstance(root_id, int) or isinstance(root_id, np.uint64):
             root_id = [root_id]
+        if isinstance(root_id, np.ndarray) or isinstance(root_id, list):
+            root_id = np.unique(root_id).astype(np.uint64)
+        else:
+            raise TypeError("root_id has to be list or uint64")
 
         endpoint_mapping = self.default_url_mapping
 
