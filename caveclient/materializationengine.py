b8d6cbac55f6bbd88e42d5d23943d90c53572591
Forrest Collman <forrest.collman@gmail.com>
2022-11-05 07:41:27 -0700

    wip live live method

--- caveclient/materializationengine.py
+++ caveclient/materializationengine.py
@@ -401,10 +401,11 @@ class MaterializatonClientV2(ClientBase):
 
     @cached(cache=TTLCache(maxsize=100, ttl=60 * 60 * 12))
     def get_table_metadata(
-        self, table_name: str, 
-        datastack_name=None, 
+        self,
+        table_name: str,
+        datastack_name=None,
         version: int = None,
-        log_warning: bool = True
+        log_warning: bool = True,
     ):
         """Get metadata about a table
 
@@ -414,10 +415,10 @@ class MaterializatonClientV2(ClientBase):
             datastack_name: str or None, optional,
                 Name of the datastack_name.
                 If None, uses the one specified in the client.
-            version (int, optional): 
+            version (int, optional):
                 version to get. If None, uses the one specified in the client.
-            log_warning (bool, optional): 
-                whether to print out warnings to the logger. 
+            log_warning (bool, optional):
+                whether to print out warnings to the logger.
                 Defaults to True.
 
         Returns:
@@ -504,7 +505,7 @@ class MaterializatonClientV2(ClientBase):
                 table_name=table,
                 datastack_name=datastack_name,
                 version=materialization_version,
-                log_warning=False
+                log_warning=False,
             )
             if md["reference_table"] is None:
                 target_table = None
@@ -655,7 +656,10 @@ class MaterializatonClientV2(ClientBase):
                             "desired resolution needs to be of length 3, for xyz"
                         )
                     vox_res = self.get_table_metadata(
-                        table, datastack_name, materialization_version, log_warning=False
+                        table,
+                        datastack_name,
+                        materialization_version,
+                        log_warning=False,
                     )["voxel_resolution"]
                     df = convert_position_columns(df, vox_res, desired_resolution)
             if metadata:
@@ -950,6 +954,119 @@ class MaterializatonClientV2(ClientBase):
 
         return df
 
+    def live_live_query(
+        self,
+        table: str,
+        timestamp: datetime,
+        filter_in_dict=None,
+        filter_out_dict=None,
+        filter_equal_dict=None,
+        filter_spatial_dict=None,
+        select_columns=None,
+        offset: int = None,
+        limit: int = None,
+        datastack_name: str = None,
+        split_positions: bool = False,
+        metadata: bool = True,
+        merge_reference: bool = True,
+        desired_resolution: Iterable = None,
+        return_pyarrow: bool = True,
+    ):
+
+        timestamp = convert_timestamp(timestamp)
+        return_df = True
+        if datastack_name is None:
+            datastack_name = self.datastack_name
+
+        endpoint_mapping = self.default_url_mapping
+        endpoint_mapping["datastack_name"] = datastack_name
+        data = {}
+        query_args = {}
+        query_args["return_pyarrow"] = True
+        query_args["merge_reference"] = False
+
+        data["table"] = table
+        data["timestamp"] = timestamp
+        url = self._endpoints["live_live_query"].format_map(endpoint_mapping)
+
+        if filter_in_dict is not None:
+            data["filter_in_dict"] = filter_in_dict
+        if filter_out_dict is not None:
+            data["filter_notin_dict"] = filter_out_dict
+        if filter_equal_dict is not None:
+            data["filter_equal_dict"] = filter_equal_dict
+        if filter_spatial_dict is not None:
+            data["filter_spatial_dict"] = filter_spatial_dict
+        if select_columns is not None:
+            data["select_columns"] = select_columns
+        if offset is not None:
+            data["offset"] = offset
+        if limit is not None:
+            assert limit > 0
+            data["limit"] = limit
+        if return_pyarrow:
+            encoding = ""
+        else:
+            encoding = "gzip"
+
+        response = self.session.post(
+            url,
+            data=json.dumps(data, cls=BaseEncoder),
+            headers={
+                "Content-Type": "application/json",
+                "Accept-Encoding": encoding,
+            },
+            params=query_args,
+            stream=~return_df,
+            verify=self.verify,
+        )
+        self.raise_for_status(response)
+
+        if desired_resolution is None:
+            desired_resolution = self.desired_resolution
+
+        with TimeIt("deserialize"):
+            with warnings.catch_warnings():
+                warnings.simplefilter(action="ignore", category=FutureWarning)
+                warnings.simplefilter(action="ignore", category=DeprecationWarning)
+                df = pa.deserialize(response.content)
+                if desired_resolution is not None:
+                    df = df.copy()
+                    if len(desired_resolution) != 3:
+                        raise ValueError(
+                            "desired resolution needs to be of length 3, for xyz"
+                        )
+                    vox_res = self.get_table_metadata(
+                        table_name=table,
+                        datastack_name=datastack_name,
+                        log_warning=False,
+                    )["voxel_resolution"]
+                    df = convert_position_columns(df, vox_res, desired_resolution)
+            if not split_positions:
+                concatenate_position_columns(df, inplace=True)
+
+        if metadata:
+            attrs = self._assemble_attributes(
+                table,
+                join_query=False,
+                filters={
+                    "inclusive": filter_in_dict,
+                    "exclusive": filter_out_dict,
+                    "equal": filter_equal_dict,
+                    "spatial": filter_spatial_dict,
+                },
+                select_columns=select_columns,
+                offset=offset,
+                limit=limit,
+                live_query=timestamp is not None,
+                timestamp=string_format_timestamp(timestamp),
+                materialization_version=None,
+                desired_resolution=desired_resolution,
+            )
+            df.attrs.update(attrs)
+
+        return df
+
     def live_query(
         self,
         table: str,
@@ -1148,7 +1265,7 @@ class MaterializatonClientV2(ClientBase):
                         table_name=table,
                         datastack_name=datastack_name,
                         version=materialization_version,
-                        log_warning=False
+                        log_warning=False,
                     )["voxel_resolution"]
                     df = convert_position_columns(df, vox_res, desired_resolution)
             if not split_positions:
