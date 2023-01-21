fa00e9b0a8d7f3aeae2afaf70a6dfaaa94be49c1
Forrest Collman <forrest.collman@gmail.com>
2023-01-20 16:43:15 -0800

    fixing doc string formatting

--- caveclient/l2cache.py
+++ caveclient/l2cache.py
@@ -86,10 +86,8 @@ class L2CacheClientLegacy(ClientBase):
         Args:
             l2_ids (list or np.ndarray): a list of level 2 ids
             attributes (list, optional): a list of attributes to retrieve.
-             Defaults to None which will return all that are available.
-             Available stats are ['area_nm2', 'chunk_intersect_count',
-             'max_dt_nm', 'mean_dt_nm', 'pca', 'pca_val',
-            'rep_coord_nm', 'size_nm3']. See docs for more description.
+                Defaults to None which will return all that are available.
+                Available stats are ['area_nm2', 'chunk_intersect_count', 'max_dt_nm', 'mean_dt_nm', 'pca', 'pca_val', 'rep_coord_nm', 'size_nm3']. See docs for more description.
 
         Returns:
             dict: keys are l2 ids, values are data
