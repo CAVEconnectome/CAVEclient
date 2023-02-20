a0f19440ea57ba5b3215f2481a11f2a61875a8af
Sven Dorkenwald <sven.dorkenwald@googlemail.com>
2023-02-20 08:54:51 -0800

    added warning about outdated version

--- caveclient/__init__.py
+++ caveclient/__init__.py
@@ -1,3 +1,17 @@
-__version__ = "4.20.2"
+__version__ = "4.20.1"
 
 from .frameworkclient import CAVEclient
+
+try:
+    import requests
+    import json
+    from packaging import version
+    import warnings
+
+    r = requests.get("https://pypi.org/pypi/caveclient/json")
+    pypi_version = json.loads(r.content)["info"]["version"]
+
+    if version.parse(pypi_version) > version.parse(__version__):
+        warnings.warn(f"New CAVEclient version available! Your version: {__version__} Latest version: {pypi_version}.", UserWarning)
+except:
+    pass
