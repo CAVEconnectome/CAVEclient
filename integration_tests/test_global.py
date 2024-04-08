2795a109241faf0a7b8cdb5d7d8f6a6bdd040ab2
Forrest Collman <forrest.collman@gmail.com>
2024-04-08 12:51:58 -0700

    fix token test

--- integration_tests/test_global.py
+++ integration_tests/test_global.py
@@ -3,6 +3,6 @@ import os
 
 
 def test_global_1():
-    client = CAVEclient(token=os.environ["CAVE_TOKEN"])
+    client = CAVEclient(auth_token=os.environ["CAVE_TOKEN"])
     datastacks = client.info.get_datastacks()
     assert len(datastacks) > 10
