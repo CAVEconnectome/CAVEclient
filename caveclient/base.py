79e76acaa2617b23689a2e3776327f90f0fc27f1
Casey Schneider-Mizell <caseysm@gmail.com>
2024-08-16 12:29:23 -0700

    improve error text

--- caveclient/base.py
+++ caveclient/base.py
@@ -433,10 +433,12 @@ def _check_version_compatibility(
         if method_constraint is not None:
             if _version_fails_constraint(self.server_version, method_constraint):
                 if isinstance(method_constraint, str):
-                    method_constraint = [method_constraint]
+                    ms_list = [method_constraint]
+                else:
+                    ms_list = method_constraint
                 msg = (
                     f"Use of method `{method.__name__}` is only permitted "
-                    f"for server version {' or '.join(method_constraint)}, your server "
+                    f"for server version {' or '.join(ms_list)}. Your server "
                     f"version is {self.server_version}. Contact your system "
                     "administrator to update the server version."
                 )
@@ -455,11 +457,13 @@ def _check_version_compatibility(
                     continue
                 elif _version_fails_constraint(self.server_version, kwarg_constraint):
                     if isinstance(kwarg_constraint, str):
-                        kwarg_constraint = [kwarg_constraint]
+                        kw_list = [kwarg_constraint]
+                    else:
+                        kw_list = kwarg_constraint
                     msg = (
                         f"Use of keyword argument `{kwarg}` in `{method.__name__}` "
                         "is only permitted "
-                        f"for server version {' or '.join(kwarg_constraint)}, your server "
+                        f"for server version {' or '.join(kw_list)}. Your server "
                         f"version is {self.server_version}. Contact your system "
                         "administrator to update the server version."
                     )
