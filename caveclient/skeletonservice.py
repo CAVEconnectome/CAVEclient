1d20d42460802d04416c0d4ecbb736e38b1eeeec
Keith Wiley <keith.wiley@alleninstitute.org>
2024-10-23 13:16:46 -0700

    ruff/lint corrections

--- caveclient/skeletonservice.py
+++ caveclient/skeletonservice.py
@@ -329,7 +329,9 @@ class SkeletonClient(ClientBase):
         self.raise_for_status(response, log_warning=log_warning)
 
         if verbose_level >= 1:
-            print(f"get_skeleton() response contains content of size {len(response.content)} bytes")
+            print(
+                f"get_skeleton() response contains content of size {len(response.content)} bytes"
+            )
 
         if output_format == "none":
             return
@@ -354,9 +356,11 @@ class SkeletonClient(ClientBase):
         if output_format == "arrayscompressed":
             return SkeletonClient.decompressBytesToDict(response.content)
         if output_format == "swc" or output_format == "swccompressed":
-            file_content = response.content.decode() \
-                if output_format == "swc" \
+            file_content = (
+                response.content.decode()
+                if output_format == "swc"
                 else SkeletonClient.decompressBytesToString(response.content)
+            )
 
             # I got the SWC column header from skeleton_plot.skel_io.py
             df = pd.read_csv(
