e51b98638774ffe9df852ef180188409433b81a9
Keith Wiley <keith.wiley@alleninstitute.org>
2025-01-23 09:47:37 -0800

    ruff cleanup

--- tests/test_skeletons.py
+++ tests/test_skeletons.py
@@ -55,20 +55,31 @@ class TestSkeletonsClient:
     @responses.activate
     def test_get_versions(self, myclient, mocker):
         metadata_url = self.sk_endpoints.get("get_versions").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200)
+        responses.add(
+            responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200
+        )
 
         result = myclient.skeleton.get_versions()
         assert result == [-1, 0, 1, 2, 3]
 
     @responses.activate
     def test_get_cache_contents(self, myclient, mocker):
-        metadata_url = self.sk_endpoints.get("get_cache_contents_via_skvn_ridprefixes").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json={
-                                                            "num_found": 1,
-                                                            "files": ["filename"],
-                                                        }, status=200)
+        metadata_url = self.sk_endpoints.get(
+            "get_cache_contents_via_skvn_ridprefixes"
+        ).format_map(sk_mapping)
+        responses.add(
+            responses.GET,
+            url=metadata_url,
+            json={
+                "num_found": 1,
+                "files": ["filename"],
+            },
+            status=200,
+        )
         metadata_url = self.sk_endpoints.get("get_versions").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200)
+        responses.add(
+            responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200
+        )
 
         result = myclient.skeleton.get_cache_contents(None, 3, 0, 0)
         assert result == {
@@ -78,21 +89,27 @@ class TestSkeletonsClient:
 
     @responses.activate
     def test_skeletons_exist(self, myclient, mocker):
-        metadata_url = self.sk_endpoints.get("skeletons_exist_via_skvn_rids_as_post").format_map(sk_mapping)
+        metadata_url = self.sk_endpoints.get(
+            "skeletons_exist_via_skvn_rids_as_post"
+        ).format_map(sk_mapping)
         data = {
             0: True,
         }
         responses.add(responses.POST, url=metadata_url, json=data, status=200)
 
         metadata_url = self.sk_endpoints.get("get_versions").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200)
-        
+        responses.add(
+            responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200
+        )
+
         result = myclient.skeleton.skeletons_exist(None, 3, 0)
         assert result == True
 
     @responses.activate
     def test_multiple_skeletons_exist(self, myclient, mocker):
-        metadata_url = self.sk_endpoints.get("skeletons_exist_via_skvn_rids_as_post").format_map(sk_mapping)
+        metadata_url = self.sk_endpoints.get(
+            "skeletons_exist_via_skvn_rids_as_post"
+        ).format_map(sk_mapping)
         data = {
             0: True,
             1: False,
@@ -100,8 +117,10 @@ class TestSkeletonsClient:
         responses.add(responses.POST, url=metadata_url, json=data, status=200)
 
         metadata_url = self.sk_endpoints.get("get_versions").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200)
-        
+        responses.add(
+            responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200
+        )
+
         result = myclient.skeleton.skeletons_exist(None, 3, 0)
         assert result == {
             0: True,
@@ -110,40 +129,40 @@ class TestSkeletonsClient:
 
     @responses.activate
     def test_get_precomputed_skeleton_info(self, myclient, mocker):
-        mocker.patch.object(myclient.l2cache, 'has_cache', return_value=True)
-            
-        metadata_url = self.sk_endpoints.get("skeleton_info_versioned").format_map(sk_mapping)
-        
+        mocker.patch.object(myclient.l2cache, "has_cache", return_value=True)
+
+        metadata_url = self.sk_endpoints.get("skeleton_info_versioned").format_map(
+            sk_mapping
+        )
+
         info = {
             "@type": "neuroglancer_skeletons",
-            "transform": [
-                1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0
+            "transform": [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+            "vertex_attributes": [
+                {"id": "radius", "data_type": "float32", "num_components": 1},
+                {"id": "compartment", "data_type": "uint8", "num_components": 1},
             ],
-            "vertex_attributes": [ {
-                    "id": "radius",
-                    "data_type": "float32",
-                    "num_components": 1
-                }, {
-                    "id": "compartment",
-                    "data_type": "uint8",
-                    "num_components": 1
-                } ] }
-        
+        }
+
         responses.add(responses.GET, url=metadata_url, json=info, status=200)
         result = myclient.skeleton.get_precomputed_skeleton_info(3, None)
         assert result == info
 
     @responses.activate
     def test_get_skeleton(self, myclient, mocker):
-        mocker.patch.object(myclient.l2cache, 'has_cache', return_value=True)
+        mocker.patch.object(myclient.l2cache, "has_cache", return_value=True)
 
-        metadata_url = self.sk_endpoints.get("get_skeleton_via_skvn_rid_fmt").format_map(sk_mapping)
+        metadata_url = self.sk_endpoints.get(
+            "get_skeleton_via_skvn_rid_fmt"
+        ).format_map(sk_mapping)
         sk = {"test_skeleton": "data"}
         dict_bytes = SkeletonClient.compressDictToBytes(sk)
         responses.add(responses.GET, url=metadata_url, body=dict_bytes, status=200)
 
         metadata_url = self.sk_endpoints.get("get_versions").format_map(sk_mapping)
-        responses.add(responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200)
-        
+        responses.add(
+            responses.GET, url=metadata_url, json=[-1, 0, 1, 2, 3], status=200
+        )
+
         result = myclient.skeleton.get_skeleton(0, None, 3, "dict")
         assert result == sk
